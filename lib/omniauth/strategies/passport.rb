6276c15323a68e3f057d38a463c7b2f96ff2fa94
Jonathon M. Abbott <jonathona@everydayhero.com.au>
2016-09-09 14:24:27 +1000

    Fix for omniauth-oauth 1.4.0
    See https://github.com/intridea/omniauth-oauth2/pull/82

--- lib/omniauth/strategies/passport.rb
+++ lib/omniauth/strategies/passport.rb
@@ -31,6 +31,10 @@ module OmniAuth
           parsed.fetch 'user', Hash.new
         end
       end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
